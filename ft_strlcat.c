c28d8131e45284e2b56c11f2d9fac727df487160
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-10 20:35:22 -0300

    ft_strlcat working

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 14:35:59 by bira              #+#    #+#             */
-/*   Updated: 2023/08/10 15:30:13 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/10 19:46:45 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,39 +18,29 @@ size_t	ft_strlcat(char *dest, const char *src, size_t catsize)
 	size_t	dest_len;
 	size_t	src_len;
 
-	index = 0;
 	dest_len = ft_strlen(dest);
 	src_len = ft_strlen(src);
-	if (dest_len > src_len)
+	if (dest_len >= catsize)
+		return (catsize + src_len);
+	index = 0;
+	while ((src[index] != '\0') && ((dest_len + index + 1) < catsize))
 	{
-		while (catsize--)
-		{
-			dest[index + dest_len] = src[index];
-			index++;
-		}
+		dest[dest_len + index] = src[index];
+		index++;
 	}
-	else
-	{
-		while (catsize--)
-		{
-			dest[dest_len] = src[index];
-			dest_len++;
-			index++;
-		}
-	}
-	dest[index + dest_len] = '\0';
+	dest[dest_len + index] = '\0';
 	return (dest_len + src_len);
 }
 /*
 int	main(void)
 {
-	char		destination[20] = "playSTEM";
-	const char	source[13] = "123456789012";
+	char		destination[9] = "playSTEM";
+	const char	source[23] = "12345678901234567890";
 	
 	printf("Original destination = %s\n", destination);
 	printf("Original      source = %s\n", source);
 	printf("\033[0;33m");
-	ft_strlcat(destination, source, 5);
+	ft_strlcat(destination, source, 23);
 	printf("Modified destination = %s\n", destination);
 	return (0);
 }*/
