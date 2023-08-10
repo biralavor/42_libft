91610c88e538fdff5dcd20537a871feb2ad19634
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-10 20:35:58 -0300

    working, but still wip...

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,32 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bira <bira@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 20:20:07 by bira              #+#    #+#             */
-/*   Updated: 2023/08/05 23:42:14 by bira             ###   ########.fr       */
+/*   Updated: 2023/08/10 20:33:34 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 char	*ft_strchr(const char *string, int c)
 {
-	char	*result;
 	size_t	index;
-	size_t	diff;
 
-	result = '\0';
 	index = 0;
-	while (string[index] != '\0')
+	if ((unsigned char)c != '\0')
 	{
-		while (string[index] != c)
+		while (string[index] != '\0')
 		{
+			if (string[index] == (unsigned char)c)
+				return ((char *)string + index);
 			index++;
-			diff++;
 		}
-		result -= *(result - diff);
-		return (result);
 	}
-	return (0);
+	if (string[index] == (unsigned char)c)
+		return ((char *)string + index);
+	return (NULL);
 }
+/*
+int	main(void)
+{
+	char	*str;
+	int		lookfor;
+
+	str = "Mussum Ipsum, cacilds vidis litro abertis.";
+	lookfor = 'o';
+	printf("str = %s\n", str);
+	printf("lookfor = '%c'\n", lookfor);
+	printf("\033[0;33m");
+	printf("Result = %s\n", ft_strchr(str, lookfor));
+	return (0);
+}*/
