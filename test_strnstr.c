afa2ae41263b1dd514f32efcd90f52be74544141
Ubirajara Meneses De Lavor <umeneses@c1r10p2.42sp.org.br>
2023-08-11 16:52:24 -0300

    wip ft_strnstr.c

--- test_strnstr.c
+++ test_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 12:49:12 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/07 17:31:51 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/11 15:30:59 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,12 @@ int	main(void)
 	char	*x1;
 	char	*x2;
 
-	x1 = "playSTEM academy";
-	x2 = "em";
-	printf("My Result = %s\n", ft_strnstr(x1, x2, 2));
-	// printf("Original function = %i\n", strnstr(x1, x2, 3));
+	x1 = "Mussum Ipsum, cacilds vidis litro abertis.";
+	x2 = "Não sou faixa preta cumpadi, sou preto inteiris, inteiris.";
+	printf(" x1 = %s\n", x1);
+	printf(" x2 = %s\n", x2);
+	printf("\033[0;33m");
+	//printf("My Result = %s\n", ft_strnstr(x1, x2, 2));
+	//printf("Original function = %i\n", strnstr(x1, x2, 3));
 	return (0);
 }
