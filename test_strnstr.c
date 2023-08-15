189b80aaa454bcb3a6cffd8d3090abc35165421f
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-15 20:09:14 -0300

    updating for strnstr function testing

--- test_strnstr.c
+++ test_strnstr.c
@@ -6,27 +6,30 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 12:49:12 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/11 19:12:08 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/15 18:13:10 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "libft.h"
+#include <string.h>
+
+#define LIMIT 15
 
 int	main(void)
 {
 	char	*string;
 	char	*needle;
+	//char	*original;
 	size_t	max_size;
 
-	string = "Mussum Ipsum, cacilds vidis litro abertis.\nNão sou faixa preta cumpadi, sou preto inteiris, inteiris";
+	string = "Mussum Ipsum, cacilds vidis litro abertis.";
 	needle = "is";
 	max_size = ft_strlen(needle);
-	printf("   string = %s\n", string);
-	printf("   needle = %s\n", needle);
+	printf("String = %s\n", string);
+	printf("Needle = %s\n", needle);
+	// original = strnstr(string, needle, LIMIT);
+	// printf("Original ft = %s\n", original);
 	printf("\033[0;33m");
-	printf("My Result = %s\n", ft_strnstr(string, needle, max_size));
+	printf("      My ft = %s\n", ft_strnstr(string, needle, max_size));
 	return (0);
 }
