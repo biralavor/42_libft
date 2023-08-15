e288cec2aa2ffe779dec68e5f40a1ed4797fe4de
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-15 20:08:33 -0300

    updating strncmp main for testing

--- test_strncmp.c
+++ test_strncmp.c
@@ -6,23 +6,23 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 12:49:12 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/07 12:52:24 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/15 19:37:25 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+#include "libft.h"
 
 int	main(void)
 {
 	char	*x1;
 	char	*x2;
 
-	x1 = "STEM";
-	x2 = "STEAM";
-	printf("My Result = %i\n", ft_strncmp(x1, x2, 3));
-	printf("Original function = %i\n", strncmp(x1, x2, 3));
+	x1 = "Mussum Ipsum, cacilds vidis litro abertis.";
+	x2 = "abertis.";
+	printf("\nx1 = %s\n", x1);
+	printf("x2 = %s\n", x2);
+	printf("Original ft = %i\n", strncmp(x1, x2, 29));
+	printf("\033[0;33m");
+	printf("  My Result = %i\n", ft_strncmp(x1, x2, 29));
 	return (0);
 }
