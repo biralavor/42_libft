5b7f3268cc0e4075879bf0f12607b7f5abf924fd
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-16 11:57:19 -0300

    ft_memset.c now working

--- ft_memset.c
+++ ft_memset.c
@@ -1,24 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   0ft_memset.c                                       :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 14:37:39 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/11 14:38:39 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/16 11:55:34 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(void *str, int c, size_t n)
+#include "libft.h"
+
+void	*ft_memset(void *str, int c, size_t size)
 {
-	
+	int				index;
+	unsigned char	*newstr;
 
-
-
-
-
-	return(str);
+	newstr = (unsigned char *)str;
+	index = 0;
+	while (size > 0)
+	{
+		newstr[index] = (unsigned char)c;
+		size--;
+		index++;
+	}
+	return (newstr);
 }
 
 /*
@@ -26,5 +33,4 @@ void	*ft_memset(void *str, int c, size_t n)
 The C library function void *memset(void *str, int c, size_t n)
 copies the character c (an unsigned char) to the first n characters
 of the string pointed to, by the argument str.
-
 */
