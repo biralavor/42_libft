abfae3de73c352bd92580badf0da04cf3b7a538d
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-16 18:41:27 -0300

    bug fix, after Francinette. But still WIP

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 15:01:58 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/15 19:39:49 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/16 18:39:09 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,14 @@ int	ft_strncmp(const char *str1, const char *str2, size_t num)
 {
 	if (num == 0)
 		return (0);
-	while (num != 0)
+	while (num > 0)
 	{
 		if (*str1 != *str2)
+		{
 			return (*(unsigned char *)str1 - *(unsigned char *)str2);
+			if (*str2 == '\0')
+				break ;
+		}
 		str1++;
 		str2++;
 		num--;
