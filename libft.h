be7bb0ef603821769f7f5b1c2849264c58b76f91
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-16 19:03:08 -0300

    updating prototypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 20:30:01 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/16 17:23:08 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/16 18:55:52 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,19 @@
 	Remember to remove them before sending to Moulinette. */
 
 void	ft_bzero(void *s, size_t n);
+void	*calloc(size_t n_items, size_t size);
 int		ft_isalnum(unsigned char content);
 int		ft_isalpha(int c);
 int		ft_isascii(int content);
 int		ft_isdigit(int input);
 int		ft_isprint(int content);
-char	*ft_strdup(const char *src);
 void	*ft_memchr(const void *str, int c, size_t n);
 int		ft_memcmp(const void *str1, const void *str2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *str1, const void *str2, size_t n);
 void	*ft_memset(void *str, int c, size_t size);
 char	*ft_strchr(const char *string, int c);
+char	*ft_strdup(const char *src);
 size_t	ft_strlcat(char *dest, const char *src, size_t catsize);
 size_t	ft_strlcpy(char *dest, const char *src, size_t dest_size);
 size_t	ft_strlen(const char *content);
