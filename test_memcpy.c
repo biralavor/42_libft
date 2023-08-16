c32bac7c270b6abf1a19e1f3cbce751ff4136bd9
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-16 12:27:01 -0300

    updating tests for ft_memcpy.c

--- test_memcpy.c
+++ test_memcpy.c
@@ -6,24 +6,24 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 19:57:10 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/08 20:05:57 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/16 12:26:36 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-
-size_t	ft_strlen(const char *content);
 
 int	main(void)
 {
-	const char	src[99] = "Project, Peers";
-	char		dest[99] = "Play, Passion";
+	char	dest[99] = "Project, Peers, Play";
+	char	src[99] = "Passion. This is playSTEM academy.";
 
-	printf("src = %s\n", src);
-	ft_memcpy(dest, src, (ft_strlen(dest) + 1));
 	printf("dest = %s\n", dest);
+	printf("src  = %s\n", src);
+	printf("\033[0;33m");
+	ft_memcpy(dest, src, 5);
+	printf("My ft_memcpy dest = %s\n", dest);
+	printf("\033[0;34m");
+	memcpy(dest, src, 5);
+	printf("  Original memcpy = %s\n", dest);
 	return (0);
 }
