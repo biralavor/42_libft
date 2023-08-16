5ad9fa3ccac128f309332611f675dfc9b4cb0c6f
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-16 15:57:26 -0300

    updating tests for ft_memmove.c

--- test_memmove.c
+++ test_memmove.c
@@ -6,22 +6,26 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 20:33:48 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/08 20:36:39 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/16 14:03:53 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
-
-void	*ft_memmove(void *dest, const void *src, size_t n);
 
 int	main(void)
 {
-	const char	src[99] = "Project, Peers";
-	char		dest[99] = "Play, Passion";
+	char	*dest;
+	char	*src;
 
-	printf("src = %s\n", src);
-	ft_memmove(dest, src, 5);
+	dest = "Project, Peers, Play";
+	src = "Passion. This is playSTEM academy.";
 	printf("dest = %s\n", dest);
+	printf("src  = %s\n", src);
+	printf("\033[0;33m");
+	// ft_memmove(dest, src, 5);
+	// printf("My ft_memcpy dest = %s\n", dest);
+	printf("\033[0;34m");
+	memmove(dest, src, 5);
+	printf("  Original memcpy = %s\n", dest);
 	return (0);
 }
