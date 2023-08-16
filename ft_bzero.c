0350f50672a6d33750a93c99d5cfedab8d0e9114
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-16 15:52:29 -0300

    ft_bzero now working

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,24 +6,29 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 14:32:58 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/11 14:35:33 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/16 15:51:32 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	ft_bzero(void *s, size_t n)
 {
-	
+	size_t			index;
+	unsigned char	*newstr;
 
-
-
-
-
-	return(s);
+	index = 0;
+	newstr = (unsigned char *)s;
+	while (n--)
+	{
+		newstr[index] = '\0';
+		index++;
+	}
+	return ;
 }
 
 /*
 #definitions:
 The bzero() function shall place n zero-valued
 bytes in the area pointed to by s.
-
 */
