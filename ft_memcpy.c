8949b719b7413fc1f9aa2ce961a8d735f55fc52f
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-16 15:55:35 -0300

    code functions improvement

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 14:37:39 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/16 12:24:32 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/16 13:49:22 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*newsrc;
 	int				index;
 
+	if (!dest && !src)
+		return (dest);
 	newdest = (unsigned char *)dest;
 	newsrc = (unsigned char *)src;
 	index = 0;
