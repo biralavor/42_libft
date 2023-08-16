b3b4728001e0e762bd58f3345e8749cfc83d482c
Ubirajara Meneses De Lavor <umeneses@c1r10p3.42sp.org.br>
2023-08-16 12:25:29 -0300

    ft_memcpy.c now working

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,35 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   0ft_memcpy.c                                       :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 14:37:39 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/11 14:46:51 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/16 12:24:32 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memcpy(void *dest, const void *scr, size_t n)
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	
+	unsigned char	*newdest;
+	unsigned char	*newsrc;
+	int				index;
 
-
-
-
-
-	return(pointer to destination);
+	newdest = (unsigned char *)dest;
+	newsrc = (unsigned char *)src;
+	index = 0;
+	while (n > 0)
+	{
+		newdest[index] = newsrc[index];
+		index++;
+		n--;
+	}
+	return (newdest);
 }
-
-/*
-#definitions:
-The C library function void *memcpy(void *dest, const void *src, size_t n)
-copies n characters from memory area src to memory area dest.
-
-dest − This is pointer to the destination array where the content
-		is to be copied, type-casted to a pointer of type void*.
-src − This is pointer to the source of data to be copied,
-		type-casted to a pointer of type void*.
-n − This is the number of bytes to be copied.
-
-*/
