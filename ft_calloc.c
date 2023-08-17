98179ca8decfe771befb08eb9e9e1f1ba878bbef
Ubirajara Meneses De Lavor <umeneses@c1r10p2.42sp.org.br>
2023-08-17 17:52:30 -0300

    ft_calloc.c now working

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,38 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 18:09:44 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/16 19:00:54 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/17 17:50:35 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*calloc(size_t n_items, size_t size)
+void	*ft_calloc(size_t n_items, size_t size)
 {
 	size_t	*ptr;
 
-	if(!n_items)
+	if (!n_items)
 		return (NULL);
-	if (size = 0)
-		return ((void *)0);
+	if (n_items == 0 || size == 0)
+		return (NULL);
 	ptr = malloc(n_items * size);
 	if (!ptr)
 		return (NULL);
 	ft_bzero(ptr, n_items * size);
 	return ((void *)ptr);
 }
+
+/*
+The calloc() function in C is used to allocate a specified
+amount of memory and then initialize it to zero. 
+The function returns a void pointer to this memory location,
+which can then be cast to the desired type. The function takes
+in two parameters that collectively specify the amount of memory 
+​to be allocated.
+The difference in malloc and calloc is that malloc does not set
+the memory to zero where as calloc sets allocated memory to zero.
+The name "calloc" stands for contiguous allocation.
+The malloc() function allocates memory and leaves
+the memory uninitialized, whereas the calloc() function allocates
+memory and initializes all bits to zero.
+*/
