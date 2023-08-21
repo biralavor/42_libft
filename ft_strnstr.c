4883a1aec38327c67e6569ab5b0736da1b6c7966
Ubirajara Meneses De Lavor <umeneses@c3r5p5.42sp.org.br>
2023-08-21 12:29:19 -0300

    removing protection, after Francinette tests

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,33 +6,31 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 15:39:44 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/16 17:53:56 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/21 12:27:09 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *tofind, size_t len)
+char	*ft_strnstr(const char *big, const char *tofind, size_t len)
 {
 	unsigned int	i;
 	unsigned int	j;
 
-	if (!*haystack)
-		return (0);
-	else if (*tofind == '\0')
-		return ((char *)haystack);
+	if ((*tofind == '\0') || !*tofind)
+		return ((char *)big);
 	else
 	{
 		i = 0;
-		while (haystack[i] && i < len)
+		while (big[i] && i < len)
 		{
 			j = 0;
-			while ((haystack[i + j] == tofind[j]) && ((i + j) < len))
+			while ((big[i + j] == tofind[j]) && ((i + j) < len))
 			{
 				j++;
 				if (tofind[j] == '\0')
-					return ((char *)&haystack[i]);
-				else if (haystack[i + j] == '\0')
+					return ((char *)&big[i]);
+				else if ((big[i + j] == '\0') || tofind[j] != big[i + j])
 					break ;
 			}
 			i++;
