b88834fd7c0c2a8780dd5fe4e218c217f23b01b6
Ubirajara Meneses De Lavor <umeneses@c3r5p5.42sp.org.br>
2023-08-21 13:47:02 -0300

    fixing after Francinette tests

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 14:35:59 by bira              #+#    #+#             */
-/*   Updated: 2023/08/10 18:35:13 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/21 13:37:21 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,19 @@
 size_t	ft_strlcpy(char *dest, const char *src, size_t dest_size)
 {
 	size_t	length;
+	size_t	src_len;
 
-	length = 0;
 	if (dest_size == 0)
 		return (ft_strlen(src));
-	else if (dest_size > 0)
+	length = 0;
+	while ((length < dest_size - 1) && src[length] != '\0')
 	{
-		while (*(src + length) != '\0')
-		{
-			if (length == dest_size)
-			{
-				length--;
-				break ;
-			}
-			*(dest + length) = *(src + length);
-			length++;
-		}
-	}
-	*(dest + length) = '\0';
-	while (*(src + length) != '\0')
+		dest[length] = src[length];
 		length++;
-	return (length);
+	}
+	dest[length] = '\0';
+	src_len = 0;
+	while (src[src_len] != '\0')
+		src_len++;
+	return (src_len);
 }
