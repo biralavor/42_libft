6cf59781d453ad24e21e99fb2b68091213990087
Ubirajara Meneses De Lavor <umeneses@c3r5p5.42sp.org.br>
2023-08-21 16:47:17 -0300

    ft_atoi almost ready

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,15 +6,38 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 15:09:36 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/17 17:54:47 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/21 16:40:17 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_atoi(const char *str)
 {
+	int	i;
+	int	signal;
+	int	result;
 
-
-	return ();
+	i = 0;
+	signal = 1;
+	result = 0;
+	while (str[i] == '\t' || str[i] == '\r' || str[i] == '\f' || \
+			str[i] == '\v' || str[i] == '\b' || str[i] == '\n' || \
+			str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i++] == '-')
+			signal *= -1;
+	}
+	if (!ft_isdigit(str[i]))
+		return (0);
+	while (ft_isdigit(str[i]))
+	{
+		result = (str[i] - 48) + (result * 10);
+		i++;
+	}
+	return (result * signal);
 }
 
 /*
