d88e0cefaee7438576d8d3651c969814dce8cfd1
biralavor <umeneses@student.42sp.org.br>
2023-08-22 13:16:47 -0300

    first implementation of t_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 19:55:19 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/21 19:59:09 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/22 13:15:24 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-		
-	return (0);
+	char	*ptr;
+	size_t	s_len;
+
+	ptr = malloc(*s);
+	if (!ptr)
+		return (NULL);
+	s_len = ft_strlen(s);
+	while (len <= s_len)
+	{
+		*ptr++ = s[start];
+		len--;
+		start++;
+	}
+	*ptr = '\0';
+	return (ptr);
 }
