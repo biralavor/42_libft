8dbfdd9036ae5d5b85021759e58c3c9a8c09311f
umeneses@student.42sp.org.br <umeneses@student.42sp.org.br>
2023-08-24 19:09:05 -0300

    updating ft_substr after Francinette. But still in WIP

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 19:55:19 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/23 11:15:35 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/24 19:05:09 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	s_len;
 	size_t	index;
 
-	ptr = malloc((len + 1) * sizeof(char));
+	s_len = ft_strlen(s);
+	if (s_len - start < len)
+		len = s_len - start;
+	ptr = ft_calloc((len + 1), sizeof(char));
 	if (ptr == NULL || s == NULL)
 		return (NULL);
-	s_len = ft_strlen(s);
 	if (start >= s_len)
 	{
 		ptr[0] = '\0';
 		return (ptr);
 	}
-	if (s_len - start < len)
-		len = s_len - start;
 	index = -1;
 	while (s[++index + start] && (index < len))
 		ptr[index] = s[start + index];
