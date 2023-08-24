f7bdafdb1d4a2a54bfe577c2b66ed3b32620b513
umeneses@student.42sp.org.br <umeneses@student.42sp.org.br>
2023-08-24 11:04:17 -0300

    updating my main tester for ft_putstr_fd.c

--- test_putstr_fd.c
+++ test_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 19:32:29 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/23 19:35:23 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/24 11:02:50 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int	main(void)
 {
 	char *string;
 
-	string = "playSTEM academy";
+	string = "https://playSTEM.academy/";
 	ft_putstr_fd(string, 1);
+	ft_putchar_fd('\n', 1);
 	return (0);
 }
