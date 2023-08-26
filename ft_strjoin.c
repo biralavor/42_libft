237eeada013a8ea252a1217493a6ca9b94a3de99
umeneses <umeneses@student.42.fr>
2023-08-26 13:59:31 -0300

    I'm became VERY happy building this onegit add ft_strjoin.c !

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bira <bira@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 12:11:43 by bira              #+#    #+#             */
-/*   Updated: 2023/08/26 12:26:34 by bira             ###   ########.fr       */
+/*   Updated: 2023/08/26 13:54:32 by bira             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,17 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	s1_len;
 	size_t	s2_len;
-	char	*ptr_s1;
-	char	*ptr_s2;
 	char	*result;
 
 	if (!s1 || !s2)
 		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	ptr_s1 = (char *)malloc((s1_len + 1) * sizeof(char));
-	ptr_s2 = (char *)malloc((s2_len + 1) * sizeof(char));
-	if (!ptr_s1 || ptr_s2)
+	result = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!result)
 		return (NULL);
-	while (result++ != s1_len + s2_len)
-	{
-		/* code */
-	}
-	
-	
-	
+	ft_strlcpy(result, s1, (s1_len + 1));
+	ft_strlcat(result, s2, (s1_len + s2_len + 1));
+	result[s1_len + s2_len + 1] = '\0';
+	return (result);
 }
