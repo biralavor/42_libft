896b7d5feb0d430f6676b9a436913cc69335fde1
umeneses@student.42sp.org.br <umeneses@student.42sp.org.br>
2023-08-28 12:20:26 -0300

    split still in WIP

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bira <bira@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 14:03:37 by bira              #+#    #+#             */
-/*   Updated: 2023/08/27 13:02:21 by bira             ###   ########.fr       */
+/*   Updated: 2023/08/28 11:18:22 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	**ft_split(char const *s, char c)
 	size_t	s_len;
 	size_t	index;
 	size_t	delimiter;
-	
+
 	if (!s || !c)
 		return (NULL);
 	s_len = ft_strlen(s);
@@ -28,12 +28,13 @@ char	**ft_split(char const *s, char c)
 		return (NULL);
 	index = -1;
 	delimiter = 0;
+	s_part1 = "";
 	while (s[++index] != c && index <= s_len)
 	{
 		s_part1[delimiter] = s[index];
 		delimiter++;
 	}
 	ft_strlcpy((char *)result, s_part1, (delimiter + 1));
-	result[delimiter] = '\0';
+	**result = '\0';
 	return (result);
 }
