8cbe5c0e62b7d98a9e5954fde753782e9f3d5646
umeneses@student.42sp.org.br <umeneses@student.42sp.org.br>
2023-08-28 16:23:35 -0300

    removing unnecessary parenthesis

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 19:55:19 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/28 15:11:39 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/28 16:00:06 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s_len - start < len)
 	{
 		len = s_len - start + 1;
-		ptr = ft_calloc((len), sizeof(char));
+		ptr = ft_calloc(len, sizeof(char));
 	}
 	if (s_len - start >= len)
 		ptr = ft_calloc((len + 1), sizeof(char));
