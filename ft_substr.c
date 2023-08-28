1ae7429a97982a78eb69c4c398883ebb55d2730e
umeneses@student.42sp.org.br <umeneses@student.42sp.org.br>
2023-08-28 15:15:43 -0300

    ft substr now working after a huge help of the Cadet @jonasv42

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bira <bira@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 19:55:19 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/27 16:35:34 by bira             ###   ########.fr       */
+/*   Updated: 2023/08/28 15:11:39 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	s_len;
 
 	s_len = ft_strlen((char *)s);
+	if (start >= s_len || s[0] == '\0')
+		return (ft_calloc(1, sizeof(char)));
 	if (s_len - start < len)
 	{
 		len = s_len - start + 1;
-		ptr = ft_calloc((s_len + 1), sizeof(char));
+		ptr = ft_calloc((len), sizeof(char));
 	}
 	if (s_len - start >= len)
 		ptr = ft_calloc((len + 1), sizeof(char));
 	if (!ptr)
 		return (NULL);
-	if (start >= s_len || s[0] == '\0')
-	{
-		free (ptr);
-		return (ft_calloc(1, sizeof(char)));
-	}
 	ft_strlcpy(ptr, (s + start), (len + 1));
 	return (ptr);
 }
