5bcd1ff063c72cc87e5310e53553d2253135017d
umeneses@student.42sp.org.br <umeneses@student.42sp.org.br>
2023-08-30 14:07:44 -0300

    its working, but still nos 100% at Francinette

--- ft_split.c
+++ ft_split.c
@@ -6,38 +6,68 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 14:03:37 by bira              #+#    #+#             */
-/*   Updated: 2023/08/29 18:35:33 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/30 14:06:47 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_event_counter(char const *s, char c)
+static size_t	ft_word_index(char const *s, char c)
 {
-	size_t	delimiter;
+	size_t	index;
+	size_t	words;
 
-	delimiter = 0;
-	while (s[delimiter] != c)
-		delimiter++;
-	return (delimiter);
+	index = 0;
+	words = 0;
+	while (s[index])
+	{
+		while (s[index] && s[index] == c)
+			index++;
+		if (s[index] && s[index] != c)
+			words++;
+		while (s[index] && s[index] != c)
+			index++;
+	}
+	return (words);
+}
+
+static size_t	ft_substr_len(char const *s, char c, size_t start)
+{
+	size_t	sub_len;
+
+	sub_len = 0;
+	while (s[start] && s[start] != c)
+	{
+		start++;
+		sub_len++;
+	}
+	return (sub_len);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	*new_s;
 	char	**result;
-	size_t	s_len;
-	size_t	delimiter;
+	size_t	index;
+	size_t	start;
+	size_t	words;
+	size_t	sub_len;
 
 	if (!s || !c)
 		return (NULL);
-	delimiter = ft_event_counter(s, c);
-	new_s = (char *)s;
-	s_len = ft_strlen(s);
-	result = ft_calloc((s_len - delimiter + 1), sizeof(char *));
+	index = 0;
+	start = 0;
+	words = ft_word_index(s, c);
+	result = (char **)ft_calloc((words + 1), sizeof(char *));
 	if (!result)
 		return (NULL);
-	result = (char **)ft_substr(new_s, delimiter, (s_len - delimiter));
-	ft_putendl_fd(&new_s[delimiter], 1);
+	while (index < words)
+	{
+		while (s[start] && s[start] == c)
+			start++;
+		sub_len = ft_substr_len(s, c, start);
+		result[index] = ft_substr(s, start, sub_len);
+		start = start + sub_len + 1;
+		index++;
+	}
 	return (result);
 }
