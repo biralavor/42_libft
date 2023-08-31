2de7168137f43b6f13c3268436e69fb5df5b2827
umeneses@student.42sp.org.br <umeneses@student.42sp.org.br>
2023-08-31 20:07:46 -0300

    updating libft.h for 42 school

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 20:30:01 by umeneses          #+#    #+#             */
-/*   Updated: 2023/08/31 12:10:54 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/08/31 19:36:35 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,7 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-/*	Library for mandatory functions */
-
-# include <string.h>
-# include <strings.h>
-# include <stdio.h>
-# include <ctype.h>
-/*	Library for testing functions.
-	Remember to remove them before sending it to Moulinette. */
+/*	Headers for mandatory functions */
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
@@ -47,6 +40,7 @@ char	*ft_strnstr(const char *haystack, const char *tofind, size_t len);
 char	*ft_strrchr(const char *str, int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+/* Mandatory Part 1 Functions */
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_itoa(int n);
@@ -59,7 +53,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_split(char const *s, char c);
 char	*ft_strtrim(char const *s1, char const *set);
-/* Part 2*/
+/* Mandatory Part 2 Functions */
 
 #endif
-/*	Functions prototypes to be tested */
