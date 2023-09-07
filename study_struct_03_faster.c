1064bc420f943d8dcea5cbcf86158c09f6879197
umeneses <umeneses@student.42sp.org.br>
2023-09-07 17:49:56 -0300

    improving knowledge

--- study_struct_03_faster.c
+++ study_struct_03_faster.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   study_struct_03.c                                  :+:      :+:    :+:   */
+/*   study_struct_03_faster.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 15:49:57 by umeneses          #+#    #+#             */
-/*   Updated: 2023/09/07 17:12:32 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/09/07 17:12:15 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,15 @@ typedef struct s_node
 	struct s_node	*next;
 }					t_node;
 
-void	*ft_add_at_end(struct s_node *head, int data)
+void	*ft_add_at_end(struct s_node *ptr, int data)
 {
 	struct s_node	*temp;
-	struct s_node	*ptr;
 
-	ptr = head;
 	temp = (struct s_node *)ft_calloc(1, sizeof(struct s_node));
 	if (!temp)
 		return (NULL);
 	temp->data = data;
 	temp->next = NULL;
-	while (ptr->next != NULL)
-		ptr = ptr->next;
 	ptr->next = temp;
 	return (temp);
 }
@@ -47,12 +43,10 @@ int	main(void)
 		return (0);
 	head->data = 45;
 	head->next = NULL;
-	ptr = (struct s_node *)ft_calloc(1, sizeof(struct s_node));
-	if (!ptr)
-		return (0);
-	ptr = ft_add_at_end(head, 98);
-	ptr = ft_add_at_end(head, 3);
-	ptr = ft_add_at_end(head, 67);
+	ptr = head;
+	ptr = ft_add_at_end(ptr, 98);
+	ptr = ft_add_at_end(ptr, 3);
+	ptr = ft_add_at_end(ptr, 67);
 	ptr = head;
 	printf("\033[0;033m");
 	printf("head data = %d\n", head->data);
--- study_struct_04.c
+++ study_struct_04.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   study_struct_03.c                                  :+:      :+:    :+:   */
+/*   study_struct_04.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 15:49:57 by umeneses          #+#    #+#             */
-/*   Updated: 2023/09/07 17:12:32 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/09/07 17:49:12 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,27 @@ typedef struct s_node
 	struct s_node	*next;
 }					t_node;
 
-void	*ft_add_at_end(struct s_node *head, int data)
+void	*ft_add_at_beg(struct s_node *head, int data)
 {
 	struct s_node	*temp;
-	struct s_node	*ptr;
 
-	ptr = head;
+	temp = (struct s_node *)ft_calloc(1, sizeof(struct s_node));
+	temp->data = data;
+	temp->next = NULL;
+	temp->next = head;
+	head = temp;
+	return (head);
+}
+
+void	*ft_add_at_end(struct s_node *ptr, int data)
+{
+	struct s_node	*temp;
+
 	temp = (struct s_node *)ft_calloc(1, sizeof(struct s_node));
 	if (!temp)
 		return (NULL);
 	temp->data = data;
 	temp->next = NULL;
-	while (ptr->next != NULL)
-		ptr = ptr->next;
 	ptr->next = temp;
 	return (temp);
 }
@@ -47,21 +55,20 @@ int	main(void)
 		return (0);
 	head->data = 45;
 	head->next = NULL;
-	ptr = (struct s_node *)ft_calloc(1, sizeof(struct s_node));
-	if (!ptr)
-		return (0);
-	ptr = ft_add_at_end(head, 98);
-	ptr = ft_add_at_end(head, 3);
-	ptr = ft_add_at_end(head, 67);
+	ptr = head;
+	ptr = ft_add_at_end(ptr, 98);
+	head = ft_add_at_beg(head, 3);
 	ptr = head;
 	printf("\033[0;033m");
 	printf("head data = %d\n", head->data);
 	while (ptr != NULL)
 	{
-		printf("ptr data  = %d\n", ptr->data);
+		printf("ptr data = %d\n", ptr->data);
 		ptr = ptr->next;
 	}
 	free (head);
 	free (ptr);
 	return (0);
 }
+
+// https://www.youtube.com/watch?v=egUHjERGf88
