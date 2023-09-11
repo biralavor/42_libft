6bbbcef79d7956999858d6164b1d6d31b4ae53ce
umeneses <umeneses@student.42sp.org.br>
2023-09-11 15:05:28 -0300

    adding ft_lstnew fuunction

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 14:34:31 by umeneses          #+#    #+#             */
-/*   Updated: 2023/09/06 14:51:51 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/09/11 12:10:51 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,12 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*new;
+	t_list	*new_node;
 
-	new = NULL;
-	new = (t_list *)ft_calloc(1, sizeof(t_list));
-	if (new == NULL)
-	{
-		return (1);
-	}
-	new->content = 1;
-	new->next = NULL;
-	return (new);
+	new_node = ft_calloc(1, sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
 }
