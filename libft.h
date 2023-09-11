3767be2f92c1bb04a5d0101f251daf62b95db511
umeneses <umeneses@student.42sp.org.br>
2023-09-11 15:04:02 -0300

    adding new bonus functions prototypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 20:30:01 by umeneses          #+#    #+#             */
-/*   Updated: 2023/09/11 15:00:24 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/09/11 15:01:50 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,14 @@ char	*ft_strtrim(char const *s1, char const *set);
 /* Mandatory Part 2 Functions */
 
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 /* Bonus Functions */
 
 #endif
